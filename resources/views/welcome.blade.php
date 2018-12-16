889995725fbac23d2abab4e2eccf388b1a5b3e67
Shubham Joshi <shubham@betalectic.com>
2018-12-16 12:53:19 +0530

    login form ui changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,11 +32,11 @@
                 width: 300px;
                 align-items: center;
                 margin: auto;
-                background: #6f6f6f;
+                background: black;
                 opacity: 0.6;
                 padding: 40px;
                 border-radius: 20px;
-                box-shadow: 2px 2px 2px 2px #171616;
+                box-shadow: 1px 1px 1px 1px #171616;
             }
             .hp-button {
                 background: transparent;
@@ -53,6 +53,30 @@
                 background-color: #fff;
                 color: black;
             }
+
+            .modal-content {
+                background: black;
+                opacity: 0.6;
+            }
+            .modal-dialog {
+                margin-top: 250px;
+            }
+            .form-button {
+                color: #000;
+                background: #b5adad;
+                border: black;
+            }
+            .form-button:hover {
+                color: #000;
+                background: #b5adad;
+            }
+            .btn-link {
+                font-weight: 500;
+                color: #fff;
+            }
+            .btn-link:hover {
+                color: #fff;
+            }
         </style>
         
     </head>
@@ -66,23 +90,24 @@
             <div class="content">
                 <div class="button-section">
                     <span class="login-button">
-                        <button class="btn hp-button" data-toggle="modal" data-target="#exampleModal">Login</button>
+                        <button class="btn hp-button" id="login_button" data-toggle="modal" data-target="#exampleModal">Login</button>
                     </span>
                     <span class="register-button">
                         <button class="btn hp-button">Register</button>
                     </span>
                 </div>
+                <!-- login modal -->
                 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" data-backdrop="false" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Login</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <h5 class="modal-title" id="exampleModalLabel" style="font-weight:700;">Go, Showcase yourself...</h5>
+                                <button type="button" id="close_login" class="close form-button" data-dismiss="modal" aria-label="Close">
                                   <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
                             <div class="modal-body">
-                                <div class="col-md-8">
+                                <div class="col-md-12">
 
                                     <form method="POST" action="{{ route('login') }}">
                                         @csrf
@@ -129,7 +154,7 @@
 
                                         <div class="form-group row mb-0">
                                             <div class="col-md-8 offset-md-4">
-                                                <button type="submit" class="btn btn-primary">
+                                                <button type="submit" class="btn btn-primary form-button">
                                                     {{ __('Login') }}
                                                 </button>
 
@@ -146,6 +171,10 @@
                         </div>
                     </div>
                 </div>
+                <!-- login modal ends -->
+                <!-- signup modal starts -->
+
+                <!-- signup modal ends -->
 
             </div>
             
@@ -153,5 +182,16 @@
         
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+        <script>
+            $('#login_button').on('click', function() {
+                console.log('clicked');
+                $('.button-section').css('display', 'none');
+            })
+
+            $('#close_login').on('click', function() {
+                $('.button-section').css('display', 'flex');
+            })
+        </script>
     </body>
 </html>
